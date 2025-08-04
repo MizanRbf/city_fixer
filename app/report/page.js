277fac6445cd8457c8a61230c967856d898d86e7
MizanRbf <mizanrbf@gmail.com>
2025-08-04 14:15:31 +0600

    issues and report page created

---
+++ app/report/page.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div className="max-w-[1500px] mx-auto px-4 min-h-[calc(100vh-346px)]">
+      {/* Title */}
+      <div className="text-center  mb-12">
+        <h1 className="text-3xl md:text-4xl font-bold text-primary mb-3 mt-5 border-4 border-t-0 border-x-0 inline-block p-2">
+          Report
+        </h1>
+      </div>
+    </div>
+  );
+};
+
+export default page;
